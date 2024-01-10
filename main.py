46505ebe777a22adfecb82b25f868677e879db02
nishanthj12 <48163429+nishanthj12@users.noreply.github.com>
2024-01-10 14:52:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def Initial Function()
-(
+def Initial Function():
   ##Sample Code
-)
-
-def feature2()
-(
+def feature1():
+  ##Sample Code for Feature 1
+def feature2():
   ##Sample Code for Feature 2
-)
