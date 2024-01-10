1bb0d5b82bc95329d0fd363ed965ee450344c7ab
nishj <nishj@altair.com>
2024-01-10 13:12:05 +0530

    Committing main.py to master branch

---
+++ main.py
@@ -0,0 +1,19 @@
+def Initial Function()
+(
+  ##Sample Code
+)
+
+def Initial Security()
+(
+  ##Sample Code
+)
+
+def feature1()
+(
+  ##Sample Code for Feature 1
+)
+
+def feature2()
+(
+  ##Sample Code for Feature 2
+)
