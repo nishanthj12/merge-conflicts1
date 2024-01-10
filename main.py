a860611a59c3c6ad066c5e22837776686a1775d9
nishj <nishj@altair.com>
2024-01-10 13:20:15 +0530

    Committing main.py from feature2 branch

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ def Initial Function()
   ##Sample Code
 )
 
-def Initial Security()
-(
-  ##Sample Code
-)
-
-def feature1()
-(
-  ##Sample Code for Feature 1
-)
-
 def feature2()
 (
   ##Sample Code for Feature 2
