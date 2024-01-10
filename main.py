adfcc3a5d2dc85356635fbc1d151dfebab6c6bee
nishanthj12 <48163429+nishanthj12@users.noreply.github.com>
2024-01-10 15:45:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 def Initial Function():
   ##Sample Code
-)
-
-def Initial Security()
-(
-  ##Sample Code
-)
-
-def feature1()
-(
-  ##Sample Code for Feature 1
-def feature1():
-  ##Sample Code for Feature 1
